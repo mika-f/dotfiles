6266bf39b47b4240ec2f3f0e92ce8a0c8045598a
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-09-01 09:31:55 +0000

    Added setup scripts

---
+++ init/vim.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# VIM Installation & Configuration Script
+
+function install_unless_installed() {
+  if rpm -qa $1 | grep -q $1; then
+    true
+  else
+    yum install -y $1
+  fi
+}
+
+install_unless_installed git
+install_unless_installed lua-devel
+install_unless_installed ncurses-devel
+
+cd ~
+
+git clone https://github.com/vim/vim.git
+cd vim
+git fetch
+git merge
+
+cd src
+make distclean
+./configure --prefix=/usr/local --with-features=huge --enable-multibyte --enable-rubyinterp --enable-luainterp --enable-cscope --enable-fail-if-missing
+make
+sudo make install
+
+cd ~
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
