ba0cbd8cce9c7510b3bdd23f6f4a6c6c9ad4bcd4
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2018-05-29 16:08:44 +0900

    Add .ripgreprc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,13 +7,19 @@ status --is-interactive; and source (plenv init -|psub)
 set -x PERL_VERSION (plenv version | awk '{print $1}')
 set -x PATH $HOME/.plenv/versions/$PERL_VERSION/bin $PATH # cpanm not link CLI tools to ~/.plenv/shims
 
+# ---------------------
 # rbenv configuration
 status --is-interactive; and source (rbenv init -|psub)
 set -x RUBY_VERSION (rbenv version | awk '{print $1}')
 
+# ---------------------
 # n configuration
 set -x N_PREFIX $HOME/n
 set -x PATH $N_PREFIX/bin $PATH
+
+# ---------------------
+# ripgrep configuration
+set -x RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
