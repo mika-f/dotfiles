c21a5c34617b1937a220d3988d40a36dcd50a00e
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-03-05 09:38:15 +0900

    feat: add Android SDK path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,38 +2,30 @@ set -x TERM screen-256color
 set -g theme_display_git_master_branch yes
 
 # ---------------------
-# *env configuration
+# anyenv configration
 if test -d {$HOME}/.anyenv/bin;
-  init-anyenv
-  install-anyenv
-else
-  # ---------------------
-  # plenv configuration
-  if command -v plenv;
-    status --is-interactive; and source (plenv init -|psub)
-    set -x PERL_VERSION (plenv version | awk '{print $1}')
-    set -x PATH $HOME/.plenv/versions/$PERL_VERSION/bin $PATH # cpanm not link CLI tools to ~/.plenv/shims
-  end
-
-  # ---------------------
-  # rbenv configuration
-  if command -v rbenv;
-    status --is-interactive; and source (rbenv init -|psub)
-    set -x RUBY_VERSION (rbenv version | awk '{print $1}')
-  end
+  status --is-interactive; and source (anyenv init -|psub)
 end
-# ---------------------
-# n configuration
-set -x N_PREFIX $HOME/n
-set -x PATH $N_PREFIX/bin $PATH
 
 # ---------------------
 # ripgrep configuration
 set -x RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
 # ---------------------
+# Android configuration
+if test -d {$HOME}/Library/Android/sdk/platform-tools;
+  set -x PATH $HOME/Library/Android/sdk/platform-tools $PATH
+end
+
+# ---------------------
 # aliases
 command -v nvim ; and alias vim='nvim'
+
+# ---------------------
+# large repos
+set -g theme_display_git           yes
+set -g theme_display_git_dirty     no
+set -g theme_display_git_untracked no
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
