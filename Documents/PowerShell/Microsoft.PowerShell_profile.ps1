cfa5f05d9f02ac237ad6321fdddd155b46e8c985
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-03 18:32:32 +0900

    chore: update Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,26 +1,28 @@
+Import-Module PSReadLine
 Import-Module 'E:\repos\github.com\Microsoft\vcpkg\scripts\posh-vcpkg'
 Invoke-Expression (&starship init powershell)
 
 # Aliases
 Set-Alias ls lsd
+Set-Alias git hub
 Set-Alias which where.exe
 
 # Replaced by Unix CoreUtils
-function Replace-Alias($cmd) {
+function Remove-Alias-When-Exists($cmd) {
   Get-Alias $cmd *>1 && Remove-Alias $cmd
 }
 
-Replace-Alias cat
-Replace-Alias cp
-Replace-Alias echo
-Replace-Alias mv
-Replace-Alias pwd
-Replace-Alias rm
+Remove-Alias-When-Exists cat
+Remove-Alias-When-Exists cp
+Remove-Alias-When-Exists echo
+Remove-Alias-When-Exists mv
+Remove-Alias-When-Exists pwd
+Remove-Alias-When-Exists rm
 
 # Call CoreUtils CLI instead of Windows System Commands
-function Call-CoreUtils($cmd) {
+function Invoke-CoreUtils($cmd) {
   Set-Variable -name "CoreUtils_$cmd" -value "function $cmd() { $cmd.exe `$args }" -scope global
   Get-Variable "CoreUtils_$cmd" -ValueOnly | Invoke-Expression
 } 
 
-Call-CoreUtils mkdir
+Invoke-CoreUtils mkdir
