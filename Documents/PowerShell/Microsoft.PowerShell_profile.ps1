499b17be631069d981f40d5cebb861be32dfcf11
MOCHIZUKI Natsune <me@mochizuki.moe>
2022-12-18 14:01:53 +0900

    Update Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,10 @@
 Import-Module PSReadLine
-Import-Module 'E:\repos\github.com\Microsoft\vcpkg\scripts\posh-vcpkg'
+# Import-Module 'E:\repos\github.com\Microsoft\vcpkg\scripts\posh-vcpkg'
 Invoke-Expression (&starship init powershell)
-
-# Aliases
-Set-Alias ls lsd
-Set-Alias git hub
-Set-Alias which where.exe
-Set-Alias open Invoke-Item
 
 # Replaced by Unix CoreUtils
 function Remove-Alias-When-Exists($cmd) {
-  Get-Alias $cmd > $null 2>&1 && Remove-Alias $cmd
+  Get-Alias $cmd > $null 2>&1 && Remove-Alias $cmd -Force
 }
 
 Remove-Alias-When-Exists cat
@@ -19,6 +13,16 @@ Remove-Alias-When-Exists echo
 Remove-Alias-When-Exists mv
 Remove-Alias-When-Exists pwd
 Remove-Alias-When-Exists rm
+Remove-Alias-When-Exists tee
+Remove-Alias-When-Exists diff
+
+# Aliases
+Set-Alias ls lsd
+Set-Alias git hub
+Set-Alias which where.exe
+Set-Alias open Invoke-Item
+Set-Alias cat bat
+
 
 # Call CoreUtils CLI instead of Windows System Commands
 function Invoke-CoreUtils($cmd) {
@@ -32,3 +36,9 @@ Invoke-CoreUtils mkdir
 function Search-History($query) {
   Get-Content (Get-PSReadLineOption).HistorySavePath | rg $query
 }
+
+# Invoke-Expression (&ast init pwsh | Out-String)
+
+
+$env:OPENSSL_CONF = ""
+$env:DOCKER_HOST = "tcp://localhost:2375"
